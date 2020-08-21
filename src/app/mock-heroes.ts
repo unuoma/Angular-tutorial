61351f8bb78271ef5d8cd9e174d43f2da243113f
Unuoma <obanyamaria@gmail.com>
2020-08-20 23:57:55 -0400

    exercise 1 and 2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Unicorn' },
+  { id: 12, name: 'Gusta' },
+  { id: 13, name: 'Magenta' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Sage' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Eclipse' },
+  { id: 20, name: 'Tornado' }
+];
