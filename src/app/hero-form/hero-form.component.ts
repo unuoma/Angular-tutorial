66a0e34df089e8b01d88605d54b4bce500e217d9
Unuoma <obanyamaria@gmail.com>
2020-09-10 22:32:06 -0400

    Validation

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Herocare } from '../hero';
+import {Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-hero-form',
@@ -10,13 +12,35 @@ import { Herocare } from '../hero';
 export class HeroFormComponent implements OnInit {
 
   carer = ['john', 'chris', 'Tina', 'Pablo', 'maria']
+  formErrors = {
+    weight: false,
+    carer: false,
+    email: false
+  }
 
-
+  formValue = {
+    weight: null,
+    carer: null,
+    email: null
+  }
 
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  validateWeight() {
+    this.formErrors.weight=this.formValue.weight < 0
+    console.log(this.formErrors.weight);
+  }
+
+  ValidateEmail() {
+    let mailFormat = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+    this.formErrors.email=!this.formValue.email.match(mailFormat)
+    console.log(this.formErrors.email);
+  }
+
+
 
 }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,9 +1,7 @@
 export interface Hero {
   id: number;
   name: string;
-  weight: number,
-  carer: string,
-  email: string;
+
 }
 export class Herocare {
   constructor(
